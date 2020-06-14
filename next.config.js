f80e9fdd41100a6acf36f2fd26567f7b46ed1d94
kevinseabourne <im_sausaged_as@hotmail.com>
2020-06-15 02:50:17 +0800

    test

--- next.config.js
+++ next.config.js
@@ -6,14 +6,14 @@ module.exports = withSourceMaps({
     config.plugins.push(new Dotenv({ silent: true }));
     return config;
   },
-  env: {
-    FIREBASE_ENDPOINT: process.env.FIREBASE_ENDPOINT,
-    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
-    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
-    SENTRY_DSN: process.env.SENTRY_DSN,
-    EMAILJS_ENDPOINT: process.env.EMAILJS_ENDPOINT,
-    EMAILJS_USERID: process.env.EMAILJS_USERID,
-    EMAILJS_TEMPLATEID: process.env.EMAILJS_TEMPLATEID,
-  },
+  // env: {
+  //   FIREBASE_ENDPOINT: process.env.FIREBASE_ENDPOINT,
+  //   FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+  //   FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+  //   FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+  //   SENTRY_DSN: process.env.SENTRY_DSN,
+  //   EMAILJS_ENDPOINT: process.env.EMAILJS_ENDPOINT,
+  //   EMAILJS_USERID: process.env.EMAILJS_USERID,
+  //   EMAILJS_TEMPLATEID: process.env.EMAILJS_TEMPLATEID,
+  // },
 });
