3ec7fa095dcef47e2386542b1c0f835d6d77abed
kevinseabourne <im_sausaged_as@hotmail.com>
2020-06-14 22:58:35 +0800

    deployment changes

--- next.config.js
+++ next.config.js
@@ -6,4 +6,13 @@ module.exports = withSourceMaps({
     config.plugins.push(new Dotenv({ silent: true }));
     return config;
   },
+  env: {
+    FIREBASE_ENDPOINT: process.env.FIREBASE_ENDPOINT,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+    SENTRY_DSN: process.env.SENTRY_DSN,
+    EMIALJS_TEMPLATEID: process.EMIALJS_TEMPLATEID,
+    EMIALJS_USERID: process.env.EMIALJS_USERID,
+  },
 });
