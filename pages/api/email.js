c5317d4d87e2810279547a56f784a35f15bc1206
kevinseabourne <im_sausaged_as@hotmail.com>
2020-06-15 02:55:05 +0800

    change

--- pages/api/email.js
+++ pages/api/email.js
@@ -3,58 +3,58 @@ import { toast } from "react-toastify";
 import emailjs from "emailjs-com";
 import http from "./httpService";
 
-// export async function sendEmail(data) {
-//   // Cypress Testing Coverage //
-//   /* istanbul ignore file */
-//   const template_params = {
-//     to_name: "Kevin Seabourne",
-//     from_name: data.firstName,
-//     reply_to: data.Email,
-//     message_html: data.Message,
-//   };
-//   return emailjs
-//     .send("outlook", "test", template_params, process.env.EMAILJS_USERID)
-//     .then((response) => {
-//       toast.success("Email Sent ✅", {
-//         position: "bottom-right",
-//         autoClose: 5000,
-//         hideProgressBar: false,
-//         closeOnClick: true,
-//         pauseOnHover: true,
-//         draggable: true,
-//       });
-//       if (response && response.status === 200) {
-//         return response;
-//       }
-//     })
-//     .catch((error) => {
-//       if (error && error.status >= 400 && error.status < 500) {
-//         toast.error("An unexpected error has occurred 😔", {
-//           position: "bottom-right",
-//           autoClose: 5000,
-//           hideProgressBar: false,
-//           closeOnClick: true,
-//           pauseOnHover: true,
-//           draggable: true,
-//         });
-//         logger.log(error);
-//       }
-//     });
-// }
-
 export async function sendEmail(data) {
+  // Cypress Testing Coverage //
+  /* istanbul ignore file */
   const template_params = {
     to_name: "Kevin Seabourne",
     from_name: data.firstName,
     reply_to: data.Email,
     message_html: data.Message,
   };
-
-  const response = await http.post(process.env.EMAILJS_ENDPOINT, {
-    service_id: "outlook",
-    template_id: process.env.EMAILJS_TEMPLATEID,
-    user_id: process.env.EMAILJS_USERID,
-    template_params: template_params,
-  });
-  return response;
+  return emailjs
+    .send("outlook", "test", template_params, process.env.EMAILJS_USERID)
+    .then((response) => {
+      toast.success("Email Sent ✅", {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
+      if (response && response.status === 200) {
+        return response;
+      }
+    })
+    .catch((error) => {
+      if (error && error.status >= 400 && error.status < 500) {
+        toast.error("An unexpected error has occurred 😔", {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
+        logger.log(error);
+      }
+    });
 }
+
+// export async function sendEmail(data) {
+//   const template_params = {
+//     to_name: "Kevin Seabourne",
+//     from_name: data.firstName,
+//     reply_to: data.Email,
+//     message_html: data.Message,
+//   };
+//
+//   const response = await http.post(process.env.EMAILJS_ENDPOINT, {
+//     service_id: "outlook",
+//     template_id: process.env.EMAILJS_TEMPLATEID,
+//     user_id: process.env.EMAILJS_USERID,
+//     template_params: template_params,
+//   });
+//   return response;
+// }
