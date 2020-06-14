ce6b57309fd7346bb08fc02eed2da7c198cfbcd5
kevinseabourne <im_sausaged_as@hotmail.com>
2020-06-15 03:42:28 +0800

    test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,27 @@
-const withSourceMaps = require("@zeit/next-source-maps");
+// const withSourceMaps = require("@zeit/next-source-maps");
 const Dotenv = require("dotenv-webpack");
 
-module.exports = withSourceMaps({
-  webpack(config, _options) {
+// module.exports = withSourceMaps({
+//   webpack(config, _options) {
+//     config.plugins.push(new Dotenv({ silent: true }));
+//     return config;
+//   },
+//   env: {
+//     FIREBASE_ENDPOINT: process.env.FIREBASE_ENDPOINT,
+//     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+//     FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+//     FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+//     SENTRY_DSN: process.env.SENTRY_DSN,
+//     EMAILJS_ENDPOINT: process.env.EMAILJS_ENDPOINT,
+//     EMAILJS_USER_ID: process.env.EMAILJS_USER_ID,
+//     EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
+//   },
+// });
+
+module.exports = {
+  webpack: (config) => {
     config.plugins.push(new Dotenv({ silent: true }));
+
     return config;
   },
   env: {
@@ -16,4 +34,4 @@ module.exports = withSourceMaps({
     EMAILJS_USER_ID: process.env.EMAILJS_USER_ID,
     EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
   },
-});
+};
