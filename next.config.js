d45f74050b2b3c84428e37b56e64e054a7a8a931
kevinseabourne <im_sausaged_as@hotmail.com>
2020-06-24 06:22:59 +0800

    added comment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  // add more font or image formats if importing them to here.
   webpack: (config, options) => {
     config.module.rules.push({
       test: /\.(png|jpg|gif|woff|woff2|otf)$/i,
