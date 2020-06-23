90a7746c2f731624f74439b5ee57a473726f91a9
kevinseabourne <im_sausaged_as@hotmail.com>
2020-06-24 06:19:49 +0800

    refactor

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -1,6 +1,6 @@
 import http from "./httpService";
 
 export async function getAllProjects() {
-  const response = await http.get(process.env.FIREBASE_ENDPOINT);
+  const response = await http.get(process.env.NEXT_PUBLIC_FIREBASE_ENDPOINT);
   return response;
 }
