4262810d9fcef75a61612d455c34e50f44edd16f
kevinseabourne <im_sausaged_as@hotmail.com>
2020-07-05 00:43:11 +0800

    created seperate component for submit button

---
+++ components/common/submitButton.js
@@ -0,0 +1,288 @@
+import React, { useState } from "react";
+import styled, { keyframes, css } from "styled-components";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+export const SubmitButton = ({ status, label, handleSubmit }) => {
+  const [tickExit, setTickExit] = useState(false);
+  const handleLoading = (status, label) => {
+    if (status === "pending" || status === "resolved") {
+      return (
+        <CSSTransition
+          in={status === "pending" || status === "resolved"}
+          classNames="loadingExitAnimation"
+          timeout={500}
+          unmountOnExit
+        >
+          <LoadingContainer viewBox="35 35 35 35">
+            {status === "resolved" && <Tick data-testid="sentCheckmarkIcon" />}
+            <Sv status={status} data-testid="loading-spinner">
+              <circle
+                id="path"
+                cx="17.5"
+                cy="17.5"
+                r="16.1"
+                fill="none"
+              ></circle>
+            </Sv>
+            {/* <Loading>
+            <Spinner status={status} data-testid="loading-spinner"></Spinner>
+            {status === "resolved" && <Tick />}
+          </Loading> */}
+          </LoadingContainer>
+        </CSSTransition>
+      );
+    } else {
+      return label;
+    }
+  };
+  const reset = status === "pending" || status === "resolved";
+  return (
+    <Button
+      id={label}
+      data-testid={label}
+      onClick={(e) => handleSubmit(status, e)}
+    >
+      <TransitionGroup component={null}>
+        {reset && (
+          <CSSTransition
+            in={reset}
+            classNames="loadingExitAnimation"
+            timeout={1500}
+            unmountOnExit
+          >
+            <LoadingContainer status={status} viewBox="35 35 35 35">
+              <Tick id="tick" data-testid="sentCheckmarkIcon" />
+              <Sv status={status} data-testid="loading-spinner">
+                <circle
+                  id="path"
+                  cx="17.5"
+                  cy="17.5"
+                  r="16.1"
+                  fill="none"
+                ></circle>
+              </Sv>
+            </LoadingContainer>
+          </CSSTransition>
+        )}
+      </TransitionGroup>
+
+      {/* {handleLoading(status, label)} */}
+    </Button>
+  );
+};
+
+export default SubmitButton;
+
+const Button = styled.button`
+  min-width: 91.3px;
+  min-height: 49px;
+  max-height: 49px;
+  font-size: 1.1em;
+  border-radius: 40px;
+  border: none;
+  color: #f5f5eb;
+  margin-top: 30px;
+  font-weight: 700;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  background-color: #08aeea;
+  background: linear-gradient(0deg, #02aab0 0%, #00cdac 100%);
+  -webkit-transform: translateY(0px) scale(1);
+  -ms-transform: translateY(0px) scale(1);
+  transform: translateY(0px) scale(1);
+  -webkit-transition: all 0.15s ease;
+  transition: all 0.15s ease;
+  &:hover {
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.2);
+    transform: scale(1) translateY(-3.5px);
+    cursor: pointer;
+  }
+  &:focus {
+    outline: 0;
+  }
+  @media (max-width: 750px) {
+    width: 100%;
+  }
+`;
+
+// try svg
+
+const rotate = keyframes`
+ 100% {
+   transform: rotate(360deg);
+ }
+`;
+
+const dash = keyframes`
+0% {
+stroke-dasharray: 1, 200;
+    stroke-dashoffset: 0;
+}
+
+50% {
+    stroke-dasharray: 89, 200;
+    stroke-dashoffset: -35;
+}
+100% {
+    stroke-dasharray: 89, 200;
+    stroke-dashoffset: -124;
+}
+`;
+
+const dashOut = keyframes`
+0% {
+stroke-dasharray: 1, 200;
+    stroke-dashoffset: 0;
+}
+100% {
+    stroke-dasharray: 200, 200;
+    stroke-dashoffset: 1;
+}
+`;
+
+const pendingAnimation = css`
+  ${dash} 1.5s ease infinite;
+`;
+
+const resolvedAnimation = css`
+  ${dashOut} 1.5s ease forwards;
+`;
+
+const rotateAnimation = css`
+  ${rotate} 2s linear infinite;
+`;
+
+const Sv = styled.svg`
+  ${"" /* animation: ${({ status }) =>
+    status === "resolved" ? "none" : rotateAnimation}; */}
+  height: 35px;
+  width: 35px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  & #path {
+    width: 35px;
+    height: 35px;
+    margin-left: 20px;
+    display: block;
+    fill: transparent;
+    stroke: white;
+    stroke-linecap: round;
+    ${"" /* stroke-dasharray: ${({ status }) => (status === "resolved" ? 1 : 283)}; */}
+    stroke-dasharray: 1;
+    stroke-dashoffset: 280;
+    stroke-width: 1.4px;
+    transform-origin: 50% 50%;
+    ${"" /* animation: ${dashOut} 1.5s ease forwards;
+
+     ${
+       "" /* animation: ${({ status }) =>
+      status === "resolved" ? resolvedAnimation : pendingAnimation};  */}
+
+    ${"" /* animation-iteration-count: ${({ status }) =>
+      status === "resolved" ? 1 : "infinite"}; */}
+  }
+`;
+
+const LoadingContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  &.loadingExitAnimation-enter {
+    opacity: 1;
+    stroke-width: 1.4px !important;
+  }
+  &.loadingExitAnimation-enter-active {
+    & #path {
+      animation: ${dashOut} 1.5s ease;
+    }
+    & #tick {
+      &::after {
+        animation: ${checkmark} 0.5s ease;
+      }
+    }
+  }
+  &.loadingExitAnimation-exit {
+  }
+  &.loadingExitAnimation-exit-active {
+    & #path {
+      animation: ${dashOut} 1.5s ease reverse !important;
+    }
+    & #tick {
+      &::after {
+        animation: ${checkmark} 0.5s easeout reverse;
+      }
+    }
+  }
+`;
+
+const checkmark = keyframes`
+0% {
+  height: 0;
+  width: 0em;
+  border-right: 2.5px solid rgba(245, 245, 247, 0);
+  border-top: 2.5px solid rgba(245, 245, 247, 0);
+}
+50% {
+    border-top: 2.5px solid rgba(245, 245, 247, 1);
+    width: 0.34em;
+    height: 0;
+
+}
+100% {
+  height: 1em;
+      width: 0.34em;
+  border-right: 2.5px solid rgba(245, 245, 247, 1);
+  border-top: 2.5px solid rgba(245, 245, 247, 1);
+}
+`;
+
+const Tick = styled.div`
+  height: 1em;
+  width: 0.34em;
+  background-color: red;
+  &::after {
+    opacity: 1;
+    height: 1em;
+    width: 0.34em;
+    transform-origin: left top;
+    border-right: 2.5px solid rgba(245, 245, 247, 1);
+    border-top: 2.5px solid rgba(245, 245, 247, 1);
+    content: "";
+    left: 0px;
+    right: 12px;
+    margin: 0 auto;
+    top: 27.6px;
+    position: absolute;
+    ${"" /* animation: ${checkmark} 0.5s ease; */}
+    transform: scaleX(-1) rotate(135deg);
+    border-top-right-radius: 9%;
+    border-top-left-radius: 1px;
+    border-bottom-right-radius: 1px;
+  }
+
+  &.loadingExitAnimation-enter {
+    &::after {
+    }
+  }
+  &.loadingExitAnimation-enter-active {
+    &::after {
+      animation: ${checkmark} 0.5s ease;
+    }
+  }
+  &.loadingExitAnimation-exit {
+    &::after {
+    }
+  }
+  &.loadingExitAnimation-exit-active {
+    &::after {
+      animation: ${checkmark} 0.5s ease-out reverse;
+    }
+  }
+`;
