940d3a84343125a92816f920b30b074fa26f3f60
kevinseabourne <kevinseabourne@hotmail.com>
2021-01-23 22:32:13 +0800

    analytics tracker

--- pages/_documents.js
+++ pages/_documents.js
@@ -41,7 +41,16 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <script
+            data-host="https://microanalytics.io"
+            data-dnt="false"
+            src="https://microanalytics.io/js/script.js"
+            id="ZwSg9rf6GA"
+            async
+            defer
+          ></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
